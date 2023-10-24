573d3a0bee73945f591b8d206c386bbb72bdae63
Yuelf12345 <109325415+Yuelf12345@users.noreply.github.com>
2023-10-24 17:37:35 +0800

    Update home.tsx

--- react-ts/src/components/home.tsx
+++ react-ts/src/components/home.tsx
@@ -1,9 +1,33 @@
 import React, { PureComponent } from 'react'
+import { Routes, Route, Navigate, NavLink } from 'react-router-dom'
+import Message from './Message'
+import News from './News'
 
 export default class Home extends PureComponent {
   render() {
     return (
-      <div>home</div>
+          <div>
+      <h2>Home组件内容</h2>
+      <div>
+        <ul className="nav nav-tabs">
+          <li>
+            {/* 嵌套路由的三种写法 */}
+            {/* <NavLink className="list-group-item" to='/home/news'>News</NavLink> */}
+            {/* <NavLink className="list-group-item" to='./news'>News</NavLink> */}
+            <NavLink className="list-group-item" to='news'>News</NavLink>
+          </li>
+          <li>
+            <NavLink className="list-group-item" to='/home/message'>Message</NavLink>
+          </li>
+        </ul>
+        {/* 指定组件呈现的位置 */}
+        <Routes>
+          <Route path='news' element={<News></News>}></Route>
+          <Route path='message' element={<Message></Message>}></Route>
+          {/* <Route path='*' element={<Navigate to={'news'} replace></Navigate>}></Route> */}
+        </Routes>
+      </div>
+    </div>
     )
   }
 }
