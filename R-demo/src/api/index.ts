31f32a6e0dd204beba1f35d847f58a36d9d0ed45
Yuelf12345 <2794127100@qq.com>
2024-06-28 15:56:05 +0800

    feat: 登录逻辑

--- R-demo/src/api/index.ts
+++ R-demo/src/api/index.ts
@@ -1,7 +1,14 @@
 import service from "../utils/request.ts";
+export const apiLogin = (data: { username: string; password: string }) => {
+  return service.request({
+    url: "/login",
+    method: "POST",
+    data,
+  });
+};
 export const apiTableDate = () => {
   return service.request({
     url: "/tableDate",
-    method: "GET"
+    method: "GET",
   });
 };
