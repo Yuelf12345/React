d19979f2fdd210a8a45f49425537f3ac5eddcfd8
Yuelf12345 <2794127100@qq.com>
2025-04-14 17:43:55 +0800

    feat: 父子组件交互eventBus

--- R-demo/src/views/Learn/Parent/index.tsx
+++ R-demo/src/views/Learn/Parent/index.tsx
@@ -1,4 +1,12 @@
-import { useRef, useState, forwardRef, useImperativeHandle } from "react";
+import {
+  useRef,
+  useState,
+  useEffect,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
+import Style from "./index.module.less";
+import { Button } from "antd";
 
 // 定义 Child 组件的 props 类型
 interface ChildProps {
@@ -25,7 +33,7 @@ const Child = forwardRef<{ event: () => void }, ChildProps>((props, ref) => {
   return (
     <div>
       <h4>Child</h4>
-      <button onClick={sendData}>Child event</button>
+      <Button onClick={sendData}>Child event</Button>
       <p>{content}</p>
     </div>
   );
@@ -52,7 +60,7 @@ const Parent = () => {
     <div>
       <h3>Parent</h3>
       <p>Time: {time}</p>
-      <button onClick={handleClick}>Parent event</button>
+      <Button onClick={handleClick}>Parent event</Button>
       <Child
         ref={ChildRef}
         content="Child content"
@@ -62,4 +70,85 @@ const Parent = () => {
   );
 };
 
-export default Parent;
+// 父子组件eventBus通信
+import mitt from "mitt";
+// 创建事件总线实例
+const eventBus = mitt();
+const Child1 = () => {
+  const [content, setContent] = useState<string>("Child content");
+
+  useEffect(() => {
+    const handleChildEvent = (data: any) => {
+      console.log("Received data from child:", data);
+      setContent(data.content);
+    };
+
+    // 订阅事件
+    eventBus.on("parentEvent", handleChildEvent);
+
+    // 清理订阅
+    return () => {
+      eventBus.off("parentEvent", handleChildEvent);
+    };
+  }, []);
+
+  const handleClick = () => {
+    eventBus.emit("childEvent", { time: Date.now() });
+  };
+
+  return (
+    <div>
+      <h4>Child</h4>
+      <Button onClick={handleClick}>Child event</Button>
+      <p>{content}</p>
+    </div>
+  );
+};
+const Parent1 = () => {
+  const [time, setTime] = useState<number>(Date.now());
+  useEffect(() => {
+    const handleChildEvent = (data: any) => {
+      console.log("Received data from child:", data);
+      setTime(data.time);
+    };
+
+    // 订阅事件
+    eventBus.on("childEvent", handleChildEvent);
+
+    // 清理订阅
+    return () => {
+      eventBus.off("childEvent", handleChildEvent);
+    };
+  }, []);
+
+  const handleClick = () => {
+    console.log("Parent event");
+    eventBus.emit("parentEvent", { content: "Child content from Parent" });
+  };
+
+  return (
+    <div>
+      <h3>Parent</h3>
+      <p>Time: {time}</p>
+      <Button onClick={handleClick}>Parent event</Button>
+      <Child1 />
+    </div>
+  );
+};
+
+const Content = () => {
+  return (
+    <>
+      <div className={Style.content}>
+        <h2>父子组件props通信</h2>
+        <Parent />
+      </div>
+      <div className={Style.content}>
+        <h2>父子组件eventBus通信</h2>
+        <Parent1 />
+      </div>
+    </>
+  );
+};
+
+export default Content;
