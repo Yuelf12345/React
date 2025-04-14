e4114af833c085ae477d322df9b9443420c061ae
Yuelf12345 <2794127100@qq.com>
2025-04-14 09:57:03 +0800

    feat：函数式组件

--- R-demo/src/views/Hooks/useState.tsx
+++ R-demo/src/views/Hooks/useState.tsx
@@ -1,52 +1,52 @@
 // 类式组件
-import { Component } from "react";
+// import { Component } from "react";
 
-interface StateInterface {
-  count: number;
-}
+// interface StateInterface {
+//   count: number;
+// }
 
-interface StateProps {}
+// interface StateProps {}
 
-class State extends Component<StateProps, StateInterface> {
-  constructor(props: StateProps) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-    this.incrementCount = this.incrementCount.bind(this);
-  }
+// class State extends Component<StateProps, StateInterface> {
+//   constructor(props: StateProps) {
+//     super(props);
+//     this.state = {
+//       count: 0,
+//     };
+//     this.incrementCount = this.incrementCount.bind(this);
+//   }
 
-  incrementCount() {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  }
+//   incrementCount() {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1>Page2_1 - Counter: {this.state.count}</h1>
-        {/* 添加一个按钮来触发计数增加 */}
-        <button onClick={this.incrementCount}>Increment</button>
-      </div>
-    );
-  }
-}
-
-export default State;
-
-// 函数式组件
-// import React, { useState } from 'react';
-
-// const State: React.FC = () => {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div>
-//       <h1>Page2_1 - Counter: {count}</h1>
-//       <button onClick={() => setCount(count + 1)}>Increment</button>
-//     </div>
-//   );
-// };
+//   render() {
+//     return (
+//       <div>
+//         <h1>Page2_1 - Counter: {this.state.count}</h1>
+//         {/* 添加一个按钮来触发计数增加 */}
+//         <button onClick={this.incrementCount}>Increment</button>
+//       </div>
+//     );
+//   }
+// }
 
 // export default State;
+
+// 函数式组件
+import React, { useState } from 'react';
+
+const State: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Page2_1 - Counter: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+
+export default State;
