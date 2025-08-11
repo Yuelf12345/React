dac4cf302b7ad0ebb071eae46dc2f4417aa1a44c
Yuelf12345 <2794127100@qq.com>
2025-08-11 17:30:44 +0800

    description

--- Knowledge/a/backtrack/0.backtrack.js
+++ Knowledge/a/backtrack/0.backtrack.js
@@ -1,3 +1,66 @@
+/**
+ * 回溯的解题流程会因问题的性质和难度而有所不同，
+ * 但通常遵循以下步骤：
+ * 1. 定义递归函数，确定递归函数的参数和返回值。
+ * 2. 符合条件时，记录结果。
+ * 3. 遍历所有选择
+ * 4. 剪枝：检查选择是否合法
+ * 5. 尝试：做出选择，更新状态
+ * 6. 进行下一轮选择
+ * 7. 回退：撤销选择，恢复到之前状态
+ *
+ */
+/**
+ * @description 回溯算法 代码结构
+ * @param {*} state 直至目前已被选择的元素
+ * @param {*} choices 输入数组中的所有元素
+ * @param {*} res 返回结果
+ */
+function backtrack(state, choices, res) {
+  // 检查是否为解
+  if (isSolution(state)) {
+    // 记录解
+    recordSolution(state, res);
+  }
+  // 遍历所有选择
+  for (const choice of choices) {
+    // 剪枝：检查选择是否合法
+    if (isValid(state, choice)) {
+      // 尝试：做出选择，更新状态
+      makeChoice(state, choice);
+      // 进行下一轮选择
+      backtrack(state, [choice.left, choice.right], res);
+      // 回退：撤销选择，恢复到之前的状态
+      undoChoice(state);
+    }
+  }
+}
+
+/* 判断当前状态是否为解 */
+function isSolution(state) {
+  return state && state[state.length - 1]?.val === 7;
+}
+
+/* 记录解 */
+function recordSolution(state, res) {
+  res.push([...state]);
+}
+
+/* 判断在当前状态下，该选择是否合法 */
+function isValid(state, choice) {
+  return choice !== null && choice.val !== 3;
+}
+
+/* 更新状态 */
+function makeChoice(state, choice) {
+  state.push(choice);
+}
+
+/* 恢复状态 */
+function undoChoice(state) {
+  state.pop();
+}
+
 class Tree {
   constructor(value) {
     this.value = value;
@@ -68,54 +131,3 @@ console.log(tree.preOrder()); // [ 1, 2, 4, 5, 3, 6 ]
 // 1. 搜索所有值为7的节点，请返回结果
 // 2. 搜索所有值为7的节点，请返回根节点到这些节点的路径。
 // 3. 搜索所有值为7的节点，请返回根节点到这些节点的路径，并要求路径中不包含值为3的节点。
-
-/**
- * @description 回溯算法
- * @param {*} state 直至目前已被选择的元素
- * @param {*} choices 输入数组中的所有元素
- * @param {*} res 返回结果
- */
-function backtrack(state, choices, res) {
-  // 检查是否为解
-  if (isSolution(state)) {
-    // 记录解
-    recordSolution(state, res);
-  }
-  // 遍历所有选择
-  for (const choice of choices) {
-    // 剪枝：检查选择是否合法
-    if (isValid(state, choice)) {
-      // 尝试：做出选择，更新状态
-      makeChoice(state, choice);
-      // 进行下一轮选择
-      backtrack(state, [choice.left, choice.right], res);
-      // 回退：撤销选择，恢复到之前的状态
-      undoChoice(state);
-    }
-  }
-}
-
-/* 判断当前状态是否为解 */
-function isSolution(state) {
-  return state && state[state.length - 1]?.val === 7;
-}
-
-/* 记录解 */
-function recordSolution(state, res) {
-  res.push([...state]);
-}
-
-/* 判断在当前状态下，该选择是否合法 */
-function isValid(state, choice) {
-  return choice !== null && choice.val !== 3;
-}
-
-/* 更新状态 */
-function makeChoice(state, choice) {
-  state.push(choice);
-}
-
-/* 恢复状态 */
-function undoChoice(state) {
-  state.pop();
-}
--- Knowledge/a/dynamic/0.dynamic.js
+++ Knowledge/a/dynamic/0.dynamic.js
@@ -7,3 +7,23 @@
  *  推导状态转移方程，
  *  确定边界条件等。
  */
+
+/**
+ * 
+function dynamic(arr1, arr2) {
+  const row = arr1.length + 1;
+  const col = arr2.length + 1;
+  const dp = new Array(row).fill().map(() => new Array(col).fill(0));
+  // 初始第一项dp数据
+  for (let j = 0; j < col; j++) {
+    let n
+    dp[0][j] = n;
+  }
+  for (let i = 1; i < length; i++) {
+    for (let j = 1; j < length; j++) {
+      // 状态转移方程
+      // dp[i][j] = 
+    }
+  }
+}
+ */
